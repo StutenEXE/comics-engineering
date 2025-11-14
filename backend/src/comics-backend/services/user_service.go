a21c26d85c0954aad86e2b1f64bc034bc02b389f
StutenEXE <alexandre.bidaux78@gmail.com>
2025-11-14 16:11:06 +0100

    [backend] fix create user & hash pwd not working

--- backend/src/comics-backend/services/user_service.go
+++ backend/src/comics-backend/services/user_service.go
@@ -9,45 +9,69 @@ import (
 
 func CreateUserService(c *gin.Context) {
 	// Post form data
-	username := c.PostForm("username")
-	email := c.PostForm("email")
-	password := c.PostForm("password")
+	var user *models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.AbortWithStatusJSON(400, gin.H{"error": "invalid request"})
+		return
+	}
+	// If email already exists
+	existingUser, err := models.GetUserByEmail(user.Email)
+	if err != nil {
+		c.AbortWithStatusJSON(500, gin.H{"error": "internal error"})
+		return
+	}
+	// Email already in use
+	if existingUser != nil {
+		c.AbortWithStatusJSON(409, gin.H{"error": "email already in use"})
+		return
+	}
 	// Hash password
-	hashedPwd, err := utils.HashPassword(password)
+	hashedPwd, err := utils.HashPassword(user.Password)
 	if err != nil {
 		c.AbortWithStatusJSON(500, gin.H{"error": "internal error"})
 		return
 	}
 	// Insert into database
-	user := &models.User{
-		Username: username,
-		Email:    email,
+	user = &models.User{
+		Username: user.Username,
+		Email:    user.Email,
 		Password: hashedPwd,
 	}
 	err = user.CreateUserInDatabase()
 	if err != nil {
-		c.AbortWithStatusJSON(500, gin.H{"error": "database error"})
+		c.AbortWithStatusJSON(500, gin.H{"error": "database error", "user": user})
 		return
 	}
 	// Authenticate user
 	middleware.CreateSession(c, user)
-	user.Password = "" // Hide password
+	// Hide password (even though it's not sent back)
+	user.Password = ""
 	c.JSON(200, gin.H{"user": user})
 }
 
 func LoginService(c *gin.Context) {
 	// Post form data
-	email := c.PostForm("email")
-	password := c.PostForm("password")
+	var loginData struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	if err := c.ShouldBindJSON(&loginData); err != nil {
+		c.AbortWithStatusJSON(400, gin.H{"error": "invalid request"})
+		return
+	}
 	// Database lookup
-	user := models.GetUserByEmail(email)
+	user, err := models.GetUserByEmail(loginData.Email)
+	if err != nil {
+		c.AbortWithStatusJSON(500, gin.H{"error": "internal error"})
+		return
+	}
 	// No user found
 	if user == nil {
 		c.AbortWithStatusJSON(401, gin.H{"error": "invalid credentials"})
 		return
 	}
 	// Check password
-	if !utils.CheckPassword(user.Password, password) {
+	if !utils.CheckPassword(user.Password, loginData.Password) {
 		c.AbortWithStatusJSON(401, gin.H{"error": "invalid credentials"})
 		return
 	}
